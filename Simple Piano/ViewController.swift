125cfb674aa8595ebbb06ac0561d0af5222d57ae
Phuc Nguyen <kufuc2604@gmail.com>
2019-07-18 15:00:23 -0400

    Complete Simple Piano App

--- Simple Piano/ViewController.swift
+++ Simple Piano/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+
+    var audioPlayer: AVAudioPlayer!
+
+    let soundNameArray = ["C4","Db4","D4","Eb4","E4","F4","Gb4","G4","Ab4","A4","Bb4","B4"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+
+    @IBAction func notePressed(_ sender: UIButton) {
+
+        playSound(noteName: soundNameArray[sender.tag-1])
+
+    }
+
+    func playSound(noteName: String){
+
+        let url = Bundle.main.url(forResource: noteName, withExtension: "aiff")
+        do {
+
+
+                audioPlayer = try AVAudioPlayer(contentsOf: url!)
+                audioPlayer.prepareToPlay()
+                audioPlayer.play()
+
+        }catch {
+            print(error)
+        }
+    }
 
 }
 
