ae1640de1561f41318a7a9bc1a0b30e690711f58
Phuc Nguyen <kufuc2604@gmail.com>
2019-07-18 08:34:39 -0400

    Initial Commit

---
+++ Simple Piano/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Piano
+//
+//  Created by Ba Phuc Nguyen on 2019-07-18.
+//  Copyright © 2019 Phuc Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
